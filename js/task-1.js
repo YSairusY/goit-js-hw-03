5cd72a64ba052c0609d0ad4705637462c75b06f3
Artur <arturmatjo@gmail.com>
2024-07-01 12:16:50 +0200

    add index.html, task 1-3 and tested code

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,21 @@
-function slugify(title) {
-  let lowerCaseTitle = title.toLowerCase();
-  let slugifyArray = [];
+function isEnoughCapacity(products, containerSize) {
+  let countResult = 0;
+  const productValueArray = Object.values(products);
 
-  for (let index = 0; index < lowerCaseTitle.length; index++) {
-    if (lowerCaseTitle[index] === " ") {
-      slugifyArray.push("-");
-    } else {
-      slugifyArray.push(lowerCaseTitle[index]);
-    }
+  for (const value of productValueArray) {
+    countResult += value;
   }
-
-  slugifiedString = slugifyArray.join("");
-  return slugifiedString;
+  if (countResult <= containerSize) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
